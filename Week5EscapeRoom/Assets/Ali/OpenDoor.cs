932207f01b6994874041fcad30492a64424e3563
Aly Sharaf <ali.charaf59@gmail.com>
2023-10-27 15:32:04 +0400

    Created working KeyPad, Door animation, instructions, Keypad object
    Press E when next to door or KeyPad, code is "ABC".

---
+++ Week5EscapeRoom/Assets/Ali/OpenDoor.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class OpenDoor : MonoBehaviour
+{
+    private Animator anim;
+
+    private bool IsAtDoor = false;
+
+    [SerializeField] private TextMeshProUGUI CodeText;
+    string codeTextValue = "";
+    public string safeCode;
+    public GameObject CodePanel;
+
+
+
+
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        CodeText.text = codeTextValue;
+
+        if(codeTextValue == safeCode)
+        {
+            anim.SetTrigger("OpenDoor");
+            CodePanel.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        if(codeTextValue.Length >= 8)
+        {
+            codeTextValue = "";
+        }
+
+        if(Input.GetKey(KeyCode.E) && IsAtDoor == true)
+        {
+            CodePanel.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            IsAtDoor = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        IsAtDoor = false;
+        CodePanel.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void AddDigit(string digit)
+    {
+        codeTextValue += digit;
+    }
+}
